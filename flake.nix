239f754909022eb179da7fc696a3e025df500634
Jeff Workman <jeff.workman@gmail.com>
2023-04-25 15:05:57 -0400

    Fix broken nix-shell

--- flake.nix
+++ flake.nix
@@ -134,6 +134,52 @@
     in {
       lib = lib;
 
+      overlays = {
+        # Overlays to add different versions `nixpkgs` into package set
+        pkgs-stable = final: prev: {
+          final.pkgs-stable = import inputs.nixpkgs-stable {
+            inherit (prev.stdenv) system;
+            inherit (nixpkgsConfig) config;
+          };
+          # inherit (final.pkgs-stable) spotify;
+        };
+
+        # Overlay useful on Macs with Apple Silicon
+        apple-silicon = final: prev:
+          optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+            # Add access to x86 packages system is running Apple Silicon
+            final.pkgs-x86 = import nixpkgs-unstable {
+              system = "x86_64-darwin";
+              inherit (nixpkgsConfig) config;
+            };
+            # inherit (final.pkgs-x86) firefox;
+          };
+
+        # Overlay to include node packages listed in `./pkgs/node-packages/package.json`
+        # Run `nix run my#nodePackages.node2nix -- -14` to update packages.
+        nodePackages = _: prev: {
+          nodePackages = prev.nodePackages
+            // import ./pkgs/node-packages { pkgs = prev; };
+        };
+
+        direnv = (final: prev: {
+          nix-direnv = prev.nix-direnv.override { enableFlakes = true; };
+        });
+
+        fastStdenv = (final: prev: {
+          final.stdenv = prev.fastStdenv.mkDerivation { name = "env"; };
+        });
+
+        optimizeZsh = (final: prev: {
+          zsh = if prev.stdenv.isDarwin then
+            prev.zsh
+          else
+            lib.my.optimizeDefault prev.zsh;
+        });
+
+        rust = inputs.rust-overlay.overlays.default;
+      };
+
       nixosConfigurations = rec {
         bootstrap-x86 = makeOverridable nixosSystem {
           system = "x86_64-linux";
@@ -267,8 +313,6 @@
             programs.zsh.prezto.prompt.theme = "steeef";
           });
       };
-
-      overlays = import ./overlays.nix { inherit lib inputs nixpkgsConfig; };
 
       sharedModules = { local-options = importModule ./options; };
 
