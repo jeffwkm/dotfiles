27d79f9549c9a971ca53f9aea7e9bd5a1986f912
Jeff Workman <jeff.workman@gmail.com>
2023-05-05 14:29:14 -0400

    notify timeouts

--- modules/linux/wayland/sway/scripts/toggle-mute.sh
+++ modules/linux/wayland/sway/scripts/toggle-mute.sh
@@ -3,7 +3,7 @@
 pamixer -t
 value=$(pamixer --get-volume-human)
 notify-send.sh --expire-time=1000 \
-               --hint int:transient:1 \
-               --hint string:image-path:audio-speakers \
-               --replace-file=$HOME/.volume-popup \
-               "Volume" "$value / 100%"
+  --hint int:transient:1 \
+  --hint string:image-path:audio-speakers \
+  --replace-file=$HOME/.volume-popup \
+  "Volume" "$value / 100%"
--- modules/linux/wayland/sway/scripts/volume-up.sh
+++ modules/linux/wayland/sway/scripts/volume-up.sh
@@ -5,7 +5,7 @@ pamixer -i 2 --allow-boost
 value=$(pamixer --get-volume)
 # --hint string:image-path:audio-speakers
 notify-send.sh --expire-time=1000 \
-   --hint int:transient:1 \
-   --hint int:value:$value \
-   --replace-file=$HOME/.volume-popup \
-   "Volume" "$value% / 100%"
+  --hint int:transient:1 \
+  --hint int:value:$value \
+  --replace-file=$HOME/.volume-popup \
+  "Volume" "$value% / 100%"
