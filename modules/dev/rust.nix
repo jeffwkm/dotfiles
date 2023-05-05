b2ceedd60486fc3b0d057ae4e6927b5607867219
Jeff Workman <jeff.workman@gmail.com>
2023-05-05 17:06:46 -0400

    rustup

--- modules/dev/rust.nix
+++ modules/dev/rust.nix
@@ -6,19 +6,26 @@ let
   inherit (modules) dev;
   cfg = config.modules.dev.rust;
 in {
-  options.modules.dev.rust = { enable = mkBoolOpt dev.enable-all; };
+  options.modules.dev.rust = {
+    enable = mkBoolOpt dev.enable-all;
+    rustup = mkBoolOpt true;
+  };
 
   config = mkIf cfg.enable {
     nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
 
     home-manager.users.${user.name} = {
-      home.packages = with pkgs; [
-        rust-bindgen
-        (rust-bin.stable.latest.default.override {
-          extensions =
-            [ "rust-src" "rust-analyzer" "rustfmt" "rls" "rust-analysis" ];
-        })
-      ];
+      home.packages = with pkgs;
+        [ rust-bindgen ] ++ (if cfg.rustup then [
+          rustup
+          rust-analyzer
+        ] else
+          [
+            (rust-bin.stable.latest.default.override {
+              extensions =
+                [ "rust-src" "rust-analyzer" "rustfmt" "rls" "rust-analysis" ];
+            })
+          ]);
     };
   };
 }
