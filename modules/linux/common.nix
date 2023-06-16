a32e8dbcbbe5a5299f67445fb5ce63e9dc430631
Jeff Workman <jeff.workman@gmail.com>
2023-06-15 20:12:56 -0400

    Use "Inter" font everywhere

--- modules/linux/common.nix
+++ modules/linux/common.nix
@@ -33,6 +33,7 @@ in {
 
     fonts = mkIf modules.fonts.enable {
       enableDefaultFonts = true;
+      fontDir.enable = true;
       fontDir.decompressFonts = true;
       fontconfig = {
         enable = true;
@@ -40,6 +41,86 @@ in {
         hinting.enable = true;
         subpixel.rgba = "rgb";
         subpixel.lcdfilter = "default";
+        defaultFonts = {
+          serif = [ "Inter:medium" "Inter" "Noto Sans" ];
+          sansSerif = [ "Inter:medium" "Inter" "Noto Sans" ];
+          monospace = [
+            "JetBrains Mono Nerd Font:medium"
+            "JetBrains Mono Nerd Font"
+            "JetBrains Mono:medium"
+            "JetBrains Mono"
+          ];
+        };
+        localConf = ''
+          <?xml version="1.0"?>
+          <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+          <fontconfig>
+            <match target="pattern">
+              <test qual="any" name="family"><string>Noto Sans</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>Inter</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>Roboto</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>Inter</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>Segoe UI</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>Inter</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>arial</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>Inter</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>Helvetica Neue</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>Inter</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>Helvetica</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>Inter</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>Liberation Sans</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>Inter</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>ui-monospace</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>JetBrainsMono Nerd Font</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>SFMono-Regular</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>JetBrainsMono Nerd Font</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>SF Mono</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>JetBrainsMono Nerd Font</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>SF Mono</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>JetBrainsMono Nerd Font</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>Menlo</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>JetBrainsMono Nerd Font</string></edit>
+            </match>
+
+            <match target="pattern">
+              <test qual="any" name="family"><string>Consolas</string></test>
+              <edit name="family" mode="prepend" binding="same"><string>JetBrainsMono Nerd Font</string></edit>
+            </match>
+          </fontconfig>
+        '';
       };
     };
 
