0350cdc8bafa12018dbb3a8bc3041566203664d2
Jeff Workman <jeff.workman@gmail.com>
2023-09-20 10:36:01 -0400

    dev/go.nix

---
+++ modules/dev/go.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+with lib;
+with lib.my;
+let
+  inherit (config) user host modules;
+  inherit (modules) dev;
+  cfg = config.modules.dev.go;
+in {
+  options.modules.dev.go = { enable = mkBoolOpt dev.enable-all; };
+
+  config = mkIf cfg.enable {
+    home-manager.users.${user.name} = {
+      home.packages = with pkgs; [
+        go
+        go2nix
+        gocode
+        golangci-lint
+        golangci-lint-langserver
+        gomodifytags
+        gopls
+        gore
+        gotests
+        gotools
+      ];
+    };
+  };
+}
