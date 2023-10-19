fcb6f2e3c1aeb8db52062df730f35c544bfae4cb
Jeff Workman <jeff.workman@gmail.com>
2023-10-19 02:28:03 -0400

    devenv cachix

--- nix-config.nix
+++ nix-config.nix
@@ -1,9 +1,13 @@
 { lib, pkgs, ... }: {
-  nix.settings.substituters =
-    [ "https://cache.nixos.org/" "https://nix-community.cachix.org" ];
+  nix.settings.substituters = [
+    "https://cache.nixos.org/"
+    "https://nix-community.cachix.org"
+    "https://devenv.cachix.org"
+  ];
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" "repl-flake" ];
