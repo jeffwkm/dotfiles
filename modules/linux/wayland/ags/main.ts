4e5cb25527c94ffde725c749a65beeee3d9d4cf2
Jeff Workman <jeff.workman@gmail.com>
2024-03-27 23:12:01 -0400

    WIP ags config

--- modules/linux/wayland/ags/main.ts
+++ modules/linux/wayland/ags/main.ts
@@ -1,30 +1,208 @@
-const time = Variable("", {
-  poll: [
-    1000,
-    function () {
-      return Date().toString();
-    },
-  ],
+import { Workspace } from "types/service/hyprland";
+
+const hyprland = await Service.import("hyprland");
+const notifications = await Service.import("notifications");
+const mpris = await Service.import("mpris");
+const audio = await Service.import("audio");
+const battery = await Service.import("battery");
+const systemtray = await Service.import("systemtray");
+
+const date = Variable("", {
+  poll: [1000, 'date "+%H:%M:%S %b %e."'],
 });
+
+// widgets can be only assigned as a child in one container
+// so to make a reuseable widget, make it a function
+// then you can simply instantiate one by calling it
+
+const Workspaces = (monitor: number) => {
+  const activeId = hyprland.active.workspace.bind("id");
+  const workspaces = hyprland.bind("workspaces").as((ws) => {
+    ws = ws.filter((w) => w.monitorID === monitor);
+    ws.sort((a, b) => a.id - b.id);
+    ws.sort((a, b) => a.monitorID - b.monitorID);
+    return ws.map(({ id, monitorID }: Workspace) =>
+      Widget.Button({
+        on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
+        child: Widget.Label({ label: `${id}` }),
+        class_name: activeId.as((i) => `${i === id ? "focused" : ""}`),
+      }),
+    );
+  });
+
+  return Widget.Box({
+    class_name: "workspaces",
+    children: workspaces,
+  });
+};
+
+const ClientTitle = (monitor: number) =>
+  Widget.Label({
+    class_name: "client-title",
+    label: hyprland.active.client.bind("title"),
+  });
+
+const Clock = () =>
+  Widget.Label({
+    class_name: "clock",
+    label: date.bind(),
+  });
+
+// we don't need dunst or any other notification daemon
+// because the Notifications module is a notification daemon itself
+const Notification = () => {
+  const popups = notifications.bind("popups");
+  return Widget.Box({
+    class_name: "notification",
+    visible: popups.as((p) => p.length > 0),
+    children: [
+      Widget.Icon({
+        icon: "preferences-system-notifications-symbolic",
+      }),
+      Widget.Label({
+        label: popups.as((p) => p[0]?.summary || ""),
+      }),
+    ],
+  });
+};
+
+const Media = () => {
+  const label = Utils.watch("", mpris, "player-changed", () => {
+    if (mpris.players[0]) {
+      const { track_artists, track_title } = mpris.players[0];
+      return `${track_artists.join(", ")} - ${track_title}`;
+    } else {
+      return "Nothing is playing";
+    }
+  });
+
+  return Widget.Button({
+    class_name: "media",
+    on_primary_click: () => mpris.getPlayer("")?.playPause(),
+    on_scroll_up: () => mpris.getPlayer("")?.next(),
+    on_scroll_down: () => mpris.getPlayer("")?.previous(),
+    child: Widget.Label({ label }),
+  });
+};
+
+const Volume = () => {
+  const icons = {
+    101: "overamplified",
+    67: "high",
+    34: "medium",
+    1: "low",
+    0: "muted",
+  };
+
+  const getIcon = () => {
+    const icon = audio.speaker.is_muted
+      ? 0
+      : [101, 67, 34, 1, 0].find((threshold) => threshold <= audio.speaker.volume * 100);
+
+    return `audio-volume-${icons[icon]}-symbolic`;
+  };
+
+  const icon = Widget.Icon({
+    icon: Utils.watch(getIcon(), audio.speaker, getIcon),
+  });
+
+  const slider = Widget.Slider({
+    hexpand: true,
+    draw_value: false,
+    on_change: ({ value }) => (audio.speaker.volume = value),
+    setup: (self) =>
+      self.hook(audio.speaker, () => {
+        self.value = audio.speaker.volume || 0;
+      }),
+  });
+
+  return Widget.Box({
+    class_name: "volume",
+    css: "min-width: 180px",
+    children: [icon, slider],
+  });
+};
+
+const BatteryLabel = () => {
+  const value = battery.bind("percent").as((p) => (p > 0 ? p / 100 : 0));
+  const icon = battery
+    .bind("percent")
+    .as((p) => `battery-level-${Math.floor(p / 10) * 10}-symbolic`);
+
+  return Widget.Box({
+    class_name: "battery",
+    visible: battery.bind("available"),
+    children: [
+      Widget.Icon({ icon }),
+      Widget.LevelBar({
+        widthRequest: 140,
+        vpack: "center",
+        value,
+      }),
+    ],
+  });
+};
+
+const SysTray = () => {
+  const items = systemtray.bind("items").as((items) =>
+    items.map((item) =>
+      Widget.Button({
+        child: Widget.Icon({ icon: item.bind("icon") }),
+        on_primary_click: (_, event) => item.activate(event),
+        on_secondary_click: (_, event) => item.openMenu(event),
+        tooltip_markup: item.bind("tooltip_markup"),
+      }),
+    ),
+  );
+
+  return Widget.Box({
+    children: items,
+  });
+};
+
+// layout of the bar
+const Left = (monitor: number) =>
+  Widget.Box({
+    spacing: 8,
+    children: [Workspaces(monitor), ClientTitle(monitor)],
+  });
+
+const Center = (monitor: number) =>
+  Widget.Box({
+    spacing: 8,
+    children: [Media(), Notification()],
+  });
+
+const Right = (monitor: number) =>
+  Widget.Box({
+    hpack: "end",
+    spacing: 8,
+    // children: [Volume(), BatteryLabel(), Clock(), SysTray()],
+    children: [SysTray(), Volume(), Clock()],
+  });
 
 const Bar = (monitor: number) =>
   Widget.Window({
+    name: `ags-${monitor}`, // name has to be unique
+    class_name: "bar",
     monitor,
-    name: `bar${monitor}`,
     anchor: ["top", "left", "right"],
     exclusivity: "exclusive",
     child: Widget.CenterBox({
-      start_widget: Widget.Label({
-        hpack: "center",
-        label: "Welcome to AGS!",
-      }),
-      end_widget: Widget.Label({
-        hpack: "center",
-        label: time.bind(),
-      }),
+      start_widget: Left(monitor),
+      center_widget: Center(monitor),
+      end_widget: Right(monitor),
+      class_name: "outer",
     }),
   });
 
 App.config({
-  windows: [Bar(0)],
+  gtkTheme: "Adwaita-dark",
+  style: `${App.configDir}/style.css`,
+  windows: [Bar(0), Bar(1)],
+});
+
+Utils.monitorFile(`${App.configDir}/style.css`, function () {
+  App.resetCss();
+  App.applyCss(App.configDir + "/style.css");
 });
